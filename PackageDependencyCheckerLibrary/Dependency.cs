ab27cbfa4843268112ed0952398e5995aac5dc3f
Anders Hesselbom <anders@winsoft.se>
2026-02-06 15:40:44 +0100

    Sync.

---
+++ PackageDependencyCheckerLibrary/Dependency.cs
@@ -0,0 +1,9 @@
+﻿namespace PackageDependencyCheckerLibrary;
+
+public class Dependency
+{
+    public string ProjectName { get; set; }
+    public string PackageName { get; set; }
+    public string PackageVersion { get; set; }
+    public string Framework { get; set; }
+}
---
+++ PackageDependencyCheckerLibrary/DependencyChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PackageDependencyCheckerLibrary;
+
+public class DependencyChecker
+{
+    private readonly string _filename;
+
+    public DependencyChecker(string filename)
+    {
+        _filename = filename;
+    }
+
+    public List<Dependency> GetDependencies()
+    {
+        var dependencies = new List<Dependency>();
+        var fileInfo = new FileInfo(_filename);
+
+        if (fileInfo.Extension.Equals(".csproj", StringComparison.OrdinalIgnoreCase))
+        {
+            
+            
+        }
+
+        return dependencies;
+    }
+}
